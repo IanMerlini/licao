63fe65c4a67789a4c78f6289089c2be816b4c8eb
IanMerlini <ian_kaltbeitzer@hotmail.com>
2023-10-16 15:45:34 -0300

    tarefa enviada

---
+++ licao.c
@@ -0,0 +1,25 @@
+#include <pthread.h>
+
+int m;  // Número de linhas da matriz
+int n;  // Número de colunas da matriz
+int thread_count;  // Número de threads
+double A[MAX_M][MAX_N];  // Matriz
+double x[MAX_N];  // Vetor
+double y[MAX_M];  // Resultado
+
+void *Pth_mat_vect(void* rank) {
+    long my_rank = (long) rank;
+    int i, j;
+    int local_m = m / thread_count;
+    int my_first_row = my_rank * local_m;
+    int my_last_row = (my_rank + 1) * local_m - 1;
+
+    for (i = my_first_row; i <= my_last_row; i++) {
+        y[i] = 0.0;
+        for (j = 0; j < n; j++) {
+            y[i] += A[i][j] * x[j];
+        }
+    }
+
+    return NULL;
+}
